edcfe980914547f9234a6d3e67c6c638db8f066b
Prathap Kumaravel <50357476+Prathap0127@users.noreply.github.com>
2024-09-27 08:31:31 +0530

    Base URL

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "";
+let BASE_URL = "https://blogspot-1-ojqn.onrender.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
